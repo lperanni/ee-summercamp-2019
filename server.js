ca5114055059ba298611734c6afe877559dcc85f
lperanni <lucianoperanni95@gmail.com>
2019-05-15 04:06:09 +0200

    fixed stuff i guess

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-
 const express = require("express")
-const path = require("path")
+
 const app = express();
+
 
 const port = 3000;
 
-app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req,res) => {
-  console.log("Delivered HTML");
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-})
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/submit", (req, res) => {
-  console.log(`Welcome ${req.body.fullname}`);
+
+app.post("/submit", (req, res) => {
+  console.log(`Welcome ${JSON.stringify(req.body)}`);
 })
 
 app.listen(port, (err,data) => {
   console.log(`Server running on port ${port}`);
 })
+
+app.use(express.static(__dirname));
