bccfb1f313620f869ed4200430fa9be9c785203a
lperanni <lucianoperanni95@gmail.com>
2019-05-16 01:34:51 +0200

    Test Heroku

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+
+const port = 3000
+
+
+app.use(express.urlencoded({ extended: true }))
+
+
+app.post("/submit", (req, res) => {
+  console.log(`Welcome ${JSON.stringify(req.body)}`)
+})
+
+app.listen(port, (err,data) => {
+  console.log(`Server running on port ${port}`)
+})
+
+app.use(express.static(path.resolve(__dirname, 'public')))
